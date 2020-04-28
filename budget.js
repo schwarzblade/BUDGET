b106fe38f64cbd3f234f9bee2a6016537a589afe
schwarzblade <you@example.com>
2020-04-28 15:18:57 +0200

    style + empty files

---
+++ budget.js
@@ -0,0 +1,11 @@
+
+
+const balanceEl = document.querySelector(".balance .value");
+const incomeTotalEl = document.querySelector(".income-total");
+const outcomeTotalEl = document.querySelector(".outcome-total");
+const incomeEl = document.querySelector("#income");
+const expenseEl = document.querySelector("#expense");
+const allEl = document.querySelector("#all");
+const incomeList = document.querySelector('#income .list');
+const expenseList = document.querySelector('#expense .list');
+const allList = document.querySelector("#all");
