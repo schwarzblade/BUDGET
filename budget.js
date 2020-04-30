b07ff7d276bc793916a1188910e42b1edce9d44a
luk <schwarzblade@gmail.com>
2020-04-30 22:32:43 +0200

    show expense and income

--- budget.js
+++ budget.js
@@ -65,11 +65,112 @@ allBtn.addEventListener("click",()=> {
 	inactive( [incomeBtn, expenseBtn ] );
 })
 
+addExpense.addEventListener("click", () => {
+	if( expenseTitle.value === '' || expenseAmount.value === '' || expenseAmount.value < 0) {
+		expenseTitle.classList.add("warning");
+		expenseAmount.classList.add('warning');
+		setTimeout(()=> expenseAmount.classList.remove('warning'),3000);
+		setTimeout(()=> expenseTitle.classList.remove("warning"),3000);
+	}
+
+	let expense = {
+		type: "expense",
+		title: expenseTitle.value,
+		amount: expenseAmount.value
+	}
+
+	ENTRY_LIST.push(expense);
+	updateUI();
+	clearInput([expenseTitle, expenseAmount]);
+
+})
 
 
+addIncome.addEventListener("click", () => {
+	if( incomeTitle.value === '' || incomeAmount.value === '' || incomeAmount.value < 0) {
+		incomeTitle.classList.add("warning");
+		incomeAmount.classList.add('warning');
+		setTimeout(()=> incomeAmount.classList.remove('warning'),3000);
+		setTimeout(()=> incomeTitle.classList.remove("warning"),3000);
+	}
+
+	let income = {
+		type: "income",
+		title: incomeTitle.value,
+		amount: incomeAmount.value
+	}
+
+	ENTRY_LIST.push(income);
+
+	updateUI();
+	clearInput([incomeTitle, incomeAmount]);
+
+
+})
 
 
 //helpers
+
+function updateUI(){
+income = calculateTotal("income",ENTRY_LIST);
+outcome = calculateTotal("outcome",ENTRY_LIST);
+balance = calculateBalance(income,outcome);
+
+
+
+clearElement([expenseList, incomeList, allList]);
+
+
+let sign = (income >= outcome) ? "$" : "-$";
+
+ENTRY_LIST.forEach( (entry,index) => {
+	if(entry.type == "expense"){
+		showEntry(expenseList,entry.type,entry.title, entry.amount, index)
+	} else if(entry.type= "income"){
+		showEntry(incomeList,entry.type,entry.title, entry.amount, index)
+	}
+	showEntry(allList,entry.type,entry.title, entry.amount, index);
+
+});
+}
+function showEntry(list,type,title,amount,id){
+	const entry = `<li id= "${id}" class="${type}">
+				<div class="entry">${title}: $${amount}</div>
+				<div id="edit"></div>
+				<div id="delete"></div>
+				</li>`;
+
+const position ="afterbegin";
+
+list.insertAdjacentHTML(position,entry);
+}
+
+
+function clearElement(elements){
+	elements.forEach( element => {
+		element.innerHTML = "";
+	} )
+}
+
+function calculateTotal(type,list){
+	let sum = 0;
+
+	list.forEach( entry => {
+		if(entry.type == type){
+			sum += entry.amount;
+		}
+	})
+	return sum;
+} 
+
+
+function calculateBalance(income,outcome){
+	return income - outcome;
+}
+
+function clearInput(inputs){
+	inputs.forEach( input => input.value = '');
+}
 
 function show(element) {
 	element.classList.remove("hide");
